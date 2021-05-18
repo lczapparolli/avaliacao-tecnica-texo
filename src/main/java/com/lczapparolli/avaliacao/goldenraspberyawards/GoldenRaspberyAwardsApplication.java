43e427dc5fdc6390a91a07980f5fd9d00e5ea261
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2021-05-18 19:08:46 -0300

    Implementing application report
    - Implementing integration test

--- src/main/java/com/lczapparolli/avaliacao/goldenraspberyawards/GoldenRaspberyAwardsApplication.java
+++ src/main/java/com/lczapparolli/avaliacao/goldenraspberyawards/GoldenRaspberyAwardsApplication.java
@@ -1,21 +1,103 @@
 package com.lczapparolli.avaliacao.goldenraspberyawards;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lczapparolli.avaliacao.goldenraspberyawards.controllers.ReportController;
+import com.lczapparolli.avaliacao.goldenraspberyawards.data.CSVDataReader;
+import com.lczapparolli.avaliacao.goldenraspberyawards.data.IRepository;
+import com.lczapparolli.avaliacao.goldenraspberyawards.models.ProducersByIntervalReport;
+
+/**
+ * Classe principal da aplicação
+ */
 @SpringBootApplication
 @RestController
 public class GoldenRaspberyAwardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoldenRaspberyAwardsApplication.class, args);
-	}
+    //region Inicializador
 
-	@GetMapping("/")
-	public String hello() {
-		return String.format("Rest server is running");
-	}
+    /**
+     * Ponto de entrada do programa, inicializa a aplicação
+     * @param args Parâmetros de execução do programa
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenRaspberyAwardsApplication.class, args);
+    }
+
+    //endregion
+
+    //region Campos
+
+    /**
+     * Repositório da aplicação, contendo os dados necessários
+     */
+    IRepository repository;
+
+    //endregion
+
+    //region Construtor
+
+    /**
+     * Construtor da aplicação, inicializa os objetos necessários
+     */
+    public GoldenRaspberyAwardsApplication() {
+        this.inicializarRepositorio();
+    }
+
+    //endregion
+
+    //region Métodos HTTP
+
+    /**
+     * Raiz da aplicação, identifica se o serviço está executando ou se houve algum erro
+     * @return Retorna o status HTTP com a mensagem correspondente
+     */
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        //Caso o repositório esteja inicializado
+        if (this.repository != null)
+            return new ResponseEntity<String>("Rest server is running", HttpStatus.OK);
+        else
+            return new ResponseEntity<String>("Repository could not be initialized", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Consulta de produtores por intervalo entre as vitórias. Listando os produtores com maior e menor intervalo
+     * @return Retorna os dados do relatório
+     */
+    @GetMapping("/reports/producers/byInterval")
+    public ResponseEntity<ProducersByIntervalReport> getProducersByInterval() {
+        //Inicializa o controller, passando o repositório como parâmetro
+        ReportController controller = new ReportController(this.repository);
+        //Obtém os dados do relatório
+        ProducersByIntervalReport result = controller.getProducersByInterval();
+        //Retorna os dados com status OK
+        return new ResponseEntity<ProducersByIntervalReport>(result, HttpStatus.OK);
+    }
+
+    //endregion
+
+    //region Métodos auxiliares
+
+    /**
+     * Inicializa o repositório de dados
+     */
+    private void inicializarRepositorio() {
+        try {
+            //Cria uma nova instância do repositório
+            this.repository = new CSVDataReader();
+        } catch (IOException e) {
+            //Caso ocorra algum erro, deixa o repositório nulo
+            this.repository = null;
+        }
+    }
+
+    //endregion
 }
--- src/test/java/com/lczapparolli/avaliacao/goldenraspberyawards/GoldenRaspberyAwardsApplicationTests.java
+++ src/test/java/com/lczapparolli/avaliacao/goldenraspberyawards/GoldenRaspberyAwardsApplicationTests.java
@@ -2,12 +2,33 @@ package com.lczapparolli.avaliacao.goldenraspberyawards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GoldenRaspberyAwardsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Consulta o relatório de intervalo de vitórias dos produtores
+     * e verifica se o retorno é o esperado
+     * @throws Exception Dispara qualquer exceção que tenha ocorrido
+     */
+    @Test
+    void getProducersByInterval() throws Exception {
+        //Executa a requisição
+        mockMvc.perform(get("/reports/producers/byInterval"))
+               //Verifica se retornou com sucesso
+               .andExpect(status().isOk()) 
+               //Verifica se o retorno do método é o valor esperado
+               .andExpect(content().string("{\"min\":[{\"producer\":\"Joel Silver\",\"interval\":1,\"previousWin\":1990,\"followingWin\":1991}],\"max\":[{\"producer\":\"Matthew Vaughn\",\"interval\":13,\"previousWin\":2002,\"followingWin\":2015}]}"));
+    }
 
 }
